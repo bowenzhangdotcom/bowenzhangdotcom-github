233145a68c8272b5ca2b7624079a12d8f9d39024
Bowen Zhang <bowenzhang@gmail.com>
2020-12-02 00:11:03 -0500

    added new project

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,8 +18,8 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile1.jpg',
-  paragraphOne: `I'm a self-taught full stack developer with experience in JavaScript, Python, & Java. Thanks for stopping by, I'm excited to share my personal projects with you!`,
-  paragraphTwo: `Currently, I am pursuing my passion for technology through the UPWARD rotational program at Yext. I graduated in 2017 from Cornell University with a B.S. in Finance & Strategy.  I am also a former management consultant with ZS Associates.`,
+  paragraphOne: `I'm a self-taught full stack developer with experience in JavaScript and Python. Thanks for stopping by, I'm excited to share my personal projects with you!`,
+  paragraphTwo: `Currently, I am pursuing my passion for technology through the UPWARD rotational program at Yext as part of the Data Science and Machine Learning team. I graduated in 2017 from Cornell University with a B.S. in Finance & Strategy and previously worked at ZS Associates as a management consultant.`,
   paragraphThree: `Outside of work, I also enjoy poker, electronic music, Super Smash Bros. and getting better at picking heavy things up and putting them down`,
   resume: 'https://www.linkedin.com/in/bowenzhangdotcom/',
 };
@@ -40,18 +40,28 @@ export const projectsData = [
   //   img: 'stonks.png',
   //   title: 'NiceStocks.Info - WIP',
   //   info: 'Daily stock price tracker with alerts set for specific price movements',
-  //   info2:
-  //     'Building with React, Node.js, Material UI and the Alphavantage API.
+  //   info2:'Building with React, Node.js, Material UI and the Alphavantage API.',
   //   url: '',
   //   repo: 'https://github.com/bowenzhangdotcom',
   // },
+  {
+    id: uuidv1(),
+    img: 'poker-preflop.png',
+    title: 'Preflop Poker Assistant',
+    info:
+      "Poker range charts for heads-up and 6max online No Limit Texas Hold'em with a RNG calculator.",
+    info2:
+      'Server built with Node.js, MongoDB, and Express.js. Front end made with React.js. Data processed and uploaded with Python.',
+    url: 'https://bowenzhang.com/preflop-poker-local/',
+    repo: 'https://github.com/bowenzhangdotcom/preflop-poker-local',
+  },
   {
     id: uuidv1(),
     img: 'covid-project.png',
     title: 'COVID-19 Tracker',
     info:
       'Global tracker for COVID-19 infections, recoveries, and deaths. Data is available individually by country and is updated in real-time.',
-    info2: 'Built with React, Node.js, Material UI, and data from the Johns Hopkins CSSE API',
+    info2: 'Built with React.js and Node.js. Data from the Johns Hopkins CSSE API',
     url: 'https://bowenzhang.com/corona-tracker/',
     repo: 'https://github.com/bowenzhangdotcom/corona-tracker',
   },
